70fe1889001cf7bae12dc0d657d8facda5628339
naufalrinaldi <60633306+naufalrinaldi@users.noreply.github.com>
2020-02-25 22:33:46 +0700

    Add files via upload

---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <form class="">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" class="form-control" name="username" v-model="input.username" placeholder="Username" />
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+             <input type="password" class="form-control" name="password" v-model="input.password" placeholder="Password" />
+        </div>
+        <button type="button" class="btn btn-primary" v-on:click="login()">Login</button>
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "Secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FD7272;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+        border-radius: 30px;
+    }
+</style>
---
+++ src/views/Secure.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="secure">
+        <h1>Login Area</h1>
+        <p>
+            You're Logged In
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Secure',
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FD7272;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+        border-radius: 30px;
+    }
+</style>
